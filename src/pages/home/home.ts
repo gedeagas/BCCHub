9ce101f1dd96d1522773d429953af9ce25bed1ee
Agastya Darma <gedeagas22@gmail.com>
2017-03-06 17:52:23 +0700

    Updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,9 @@ import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Progdas} from '../../providers/progdas';
+import { TouchID } from 'ionic-native';
+import { FingerprintAIO } from 'ionic-native';
+import { AlertController } from 'ionic-angular';
 
 
 @Component({
@@ -14,12 +17,44 @@ export class HomePage {
   items: any;
     posts: any;
     modul:any;
-  constructor(public navCtrl: NavController, private progdas: Progdas) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, private progdas: Progdas) {
 
   }
 
+  
+
   ngOnInit(){
     this.getPost(11);
+  }
+
+  check(){
+    console.log('check');
+    FingerprintAIO.isAvailable().then(result =>{
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  show(){
+    console.log('show');
+    FingerprintAIO.show({
+     clientId: "Fingerprint-Demo",
+      clientSecret: "password"
+      }).then(result => {
+      console.log(result);
+      }).catch(err => {
+      console.log(err);
+      });
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Auth',
+      subTitle: 'Berhasil Login Menggunakan Fingerprint',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
   getPost(category){
